e44309a4f351da934da8cc3ce3c2792eae6678da
SathishDindgur <sathish236@gmail.com>
2025-04-04 11:20:29 +0530

    Product POM class is added

---
+++ ComcastCRMGUIFramework/src/main/java/com/comcast/crm/objectrepositoryutility/Products.java
@@ -0,0 +1,17 @@
+package com.comcast.crm.objectrepositoryutility;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class Products 
+{
+	
+	@FindBy(xpath="//img[@alt='Create Product...']")
+	private WebElement createProductButton;
+
+	public WebElement getCreateProductButton() 
+	{
+		return createProductButton;
+	}
+
+}
